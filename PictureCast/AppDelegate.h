eb5f4cfeb7ccb5980d663e2e97909a0320b66876
Evan Hsu <evanjhsu@umich.edu>
2014-02-09 00:14:35 -0500

    Initial Commit

---
+++ PictureCast/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PictureCast
+//
+//  Created by Evan Hsu on 2/9/14.
+//  Copyright (c) 2014 EECS 441. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PictureCastTests/PictureCastTests.m
@@ -0,0 +1,34 @@
+//
+//  PictureCastTests.m
+//  PictureCastTests
+//
+//  Created by Evan Hsu on 2/9/14.
+//  Copyright (c) 2014 EECS 441. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PictureCastTests : XCTestCase
+
+@end
+
+@implementation PictureCastTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
